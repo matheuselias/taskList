0b51ae692de2fb4064ba12381526b8ca18114183
Matheus <matheus__e@hotmail.com>
2016-06-01 19:27:27 -0300

    Iniciado projeto no lado do cliente com yeoman angular

---
+++ client/app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+angular
+  .module('taskListApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ client/app/scripts/controllers/task.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('taskListApp')
+  .controller('taskCtrl', function ($scope) {
+  });
