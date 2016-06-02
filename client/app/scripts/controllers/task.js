62a16ebb6f859128f1bfce161ee3008271041609
Matheus <matheus__e@hotmail.com>
2016-06-02 00:17:40 -0300

    Adicionado opção de alterar status da task marcando o checkbox dela

--- client/app/scripts/controllers/task.js
+++ client/app/scripts/controllers/task.js
@@ -1,44 +1,53 @@
-  'use strict';
+'use strict';
 
-  angular.module('taskListApp')
-    .controller('taskCtrl', function ($scope, taskFactory) {
-      $scope.tasks = [];
+angular.module('taskListApp')
+  .controller('taskCtrl', function ($scope, taskFactory) {
+    $scope.tasks = [];
 
-      var getTasks = function(){
-        taskFactory.query({id: ''},
-          function(data){
-            $scope.tasks = data;
-          },
-          function(data){
-            alert('Ocorreu um problema ao carregar as Tasks!');
-          });
-      };
+    var getTasks = function(){
+      taskFactory.query({id: ''},
+        function(data){
+          $scope.tasks = data;
+        },
+        function(data){
+          alert('Ocorreu um problema ao carregar as Tasks!');
+        });
+    };
 
-      $scope.createTask = function(){
-        taskFactory.save({task: $scope.task},
-          function(){
-            getTasks();
-            $scope.task = "";
-          },
-          function(data){
-            alert('Ocorreu um problema ao adicionar a Task!');
-          }
-        );
-      };
-
-      $scope.removeTask = function(task){
-        // taskFactory.remove({id: task.id});
-        // getTasks();
-        if (confirm('Realmente deseja excluir a task ' + task.title + '?')) {
-            taskFactory.remove({id: task.id},
-                function() {
-                    getTasks();
-                },
-                function(data) {
-                    alert('Ocorreu um problema ao remover a task!');
-                }
-            );
+    $scope.createTask = function(){
+      taskFactory.save({task: $scope.task},
+        function(){
+          getTasks();
+          $scope.task = "";
+        },
+        function(data){
+          alert('Ocorreu um problema ao adicionar a Task!');
         }
-      };
-      getTasks();
-    });
+      );
+    };
+
+    $scope.removeTask = function(task){
+      if (confirm('Realmente deseja excluir a task ' + task.title + '?')) {
+          taskFactory.remove({id: task.id},
+              function() {
+                  getTasks();
+              },
+              function(data) {
+                  alert('Ocorreu um problema ao remover a task!');
+              }
+          );
+      }
+    };
+
+    $scope.updateTask = function(task){
+      taskFactory.update({id: task.id}, {task: task},
+      function(){
+        
+      },
+      function(data){
+        alert('Ocorreu um problema ao alterar o status da task!');
+      });
+    };
+
+    getTasks();
+  });
