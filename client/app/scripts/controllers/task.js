75d81527462739688e36c331629700bb5a9edf97
Matheus <matheus__e@hotmail.com>
2016-06-01 23:53:25 -0300

    Adicionado opção para excluir task e alterado exibição do status

--- client/app/scripts/controllers/task.js
+++ client/app/scripts/controllers/task.js
@@ -1,21 +1,44 @@
-'use strict';
+  'use strict';
 
-angular.module('taskListApp')
-  .controller('taskCtrl', function ($scope, taskFactory) {
-    $scope.tasks = [];
+  angular.module('taskListApp')
+    .controller('taskCtrl', function ($scope, taskFactory) {
+      $scope.tasks = [];
 
-    var getTasks = function(){
-      taskFactory.query({id: ''}, function(data){
-          $scope.tasks = data;
-      });
-    };
+      var getTasks = function(){
+        taskFactory.query({id: ''},
+          function(data){
+            $scope.tasks = data;
+          },
+          function(data){
+            alert('Ocorreu um problema ao carregar as Tasks!');
+          });
+      };
 
-    $scope.createTask = function(){
-      taskFactory.save({task: $scope.task}, function(){
-        $scope.task = [];
-        getTasks();
-      });
-    };
+      $scope.createTask = function(){
+        taskFactory.save({task: $scope.task},
+          function(){
+            getTasks();
+            $scope.task = "";
+          },
+          function(data){
+            alert('Ocorreu um problema ao adicionar a Task!');
+          }
+        );
+      };
 
-    getTasks();
-  });
+      $scope.removeTask = function(task){
+        // taskFactory.remove({id: task.id});
+        // getTasks();
+        if (confirm('Realmente deseja excluir a task ' + task.title + '?')) {
+            taskFactory.remove({id: task.id},
+                function() {
+                    getTasks();
+                },
+                function(data) {
+                    alert('Ocorreu um problema ao remover a task!');
+                }
+            );
+        }
+      };
+      getTasks();
+    });
